6c2987a85b02959922f5e82e92c749aee96b9290
Rutvi Sukhadia <rutmahee2015@gmail.com>
2021-01-16 22:36:17 +0530

    Back end complete

--- app/src/main/java/com/woc/woc30_android_mid_evaluation_rutvi_sukhadia/MainActivity.java
+++ app/src/main/java/com/woc/woc30_android_mid_evaluation_rutvi_sukhadia/MainActivity.java
@@ -2,13 +2,49 @@ package com.woc.woc30_android_mid_evaluation_rutvi_sukhadia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String DAY="com.woc.android_timetable.DAY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showTimeTable(View view)
+    {
+        Intent intent = new Intent(this, SubActivity.class);
+        String day="";
+        if(findViewById(R.id.btn_mon) == view)
+        {
+            day = "Monday";
+        }
+        else if(findViewById(R.id.btn_tues) == view)
+        {
+            day = "Tuesday";
+        }
+        else if(findViewById(R.id.btn_we) == view)
+        {
+            day = "Wednesday";
+        }
+        else if(findViewById(R.id.btn_thurs) == view)
+        {
+            day = "Thursday";
+        }
+        if(findViewById(R.id.btn_fri) == view)
+        {
+            day = "Friday";
+        }
+        else if(findViewById(R.id.btn_sat) == view)
+        {
+            day = "Saturday";
+        }
+        intent.putExtra(DAY, day);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/woc/woc30_android_mid_evaluation_rutvi_sukhadia/SubActivity.java
+++ app/src/main/java/com/woc/woc30_android_mid_evaluation_rutvi_sukhadia/SubActivity.java
@@ -2,7 +2,10 @@ package com.woc.woc30_android_mid_evaluation_rutvi_sukhadia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -10,5 +13,81 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        Intent intent = getIntent();
+        String day = intent.getStringExtra(MainActivity.DAY);
+
+        // Set text inside textview to message
+        TextView textView = findViewById(R.id.txt_head);
+        if(day.equalsIgnoreCase("Monday")) {
+            textView.setText("Monday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("Computer Networks");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("Systems Programming");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("Object Oriented Programming Lab");
+        }
+        else if(day.equalsIgnoreCase("Tuesday")) {
+            textView.setText("Tuesday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("Object Oriented Programming");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("Web Programming");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("Systems Programming Lab");
+        }
+        else if(day.equalsIgnoreCase("Wednesday")) {
+            textView.setText("Wednesday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("Systems Programming");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("Computer Network Lab");
+        }
+        else if(day.equalsIgnoreCase("Thursday")) {
+            textView.setText("Thursday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("Object Oriented Programming");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("Software Engineering");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("Computer Networks");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("Web Programming Lab");
+        }
+        else if(day.equalsIgnoreCase("Friday")) {
+            textView.setText("Friday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("Web Programming");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("Software Engineering");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("Software Engineering Lab");
+        }
+        else if(day.equalsIgnoreCase("Saturday")) {
+            textView.setText("Saturday's Time table");
+            textView = findViewById(R.id.txt_first);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_second);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_third);
+            textView.setText("-");
+            textView = findViewById(R.id.txt_lab);
+            textView.setText("-");
+        }
+
+
     }
+
+
 }
