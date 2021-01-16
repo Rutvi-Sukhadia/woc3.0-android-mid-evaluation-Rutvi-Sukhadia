6ceafc78d50493bbfbe8ac4f0b7d8b816a3abdd9
Rutvi Sukhadia <rutmahee2015@gmail.com>
2021-01-16 15:59:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/woc/woc30_android_mid_evaluation_rutvi_sukhadia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.woc.woc30_android_mid_evaluation_rutvi_sukhadia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
